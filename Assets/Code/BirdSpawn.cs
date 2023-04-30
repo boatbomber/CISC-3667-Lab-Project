2caf9baacc94f4ac02bd6fde87accf62d37ea0f7
boatbomber <zack@boatbomber.com>
2023-04-30 13:21:38 -0400

    Spawn birds faster each level

--- Assets/Code/BirdSpawn.cs
+++ Assets/Code/BirdSpawn.cs
@@ -8,16 +8,18 @@ public class BirdSpawn : MonoBehaviour
     [SerializeField]
     GameObject bird;
 
+    private float timeSinceSpawn = 0.0f;
+    private float spawnFreq = 2.0f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        // Every 2 seconds, Instantiate a bird
-        InvokeRepeating("SpawnBird", 0.0f, 2.0f);
-    }
+    void Start() { }
 
     void SpawnBird()
     {
-        if (SceneManager.GetActiveScene().name == "Menu")
+        if (
+            SceneManager.GetActiveScene().name == "Menu"
+            || SceneManager.GetActiveScene().name == "Help"
+        )
             return;
 
         Vector2 position = new Vector2(Random.Range(-3, 3), Random.Range(0.0f, 1.0f));
@@ -25,5 +27,16 @@ public class BirdSpawn : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        spawnFreq = 0.5f + 3.0f / SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(spawnFreq);
+
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn > spawnFreq)
+        {
+            SpawnBird();
+            timeSinceSpawn = 0.0f;
+        }
+    }
 }
